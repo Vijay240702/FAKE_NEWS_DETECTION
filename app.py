7f2c9f45985a5e7b2de37233eb5009c18b622355
Vijay240702 <vijaykumartsk24@gmail.com>
2026-04-13 14:58:12 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,21 @@
+import streamlit as st
+import pickle
+
+model = pickle.load(open("model.pkl", "rb"))
+vectorizer = pickle.load(open("vectorizer.pkl", "rb"))
+
+def predict_news(text):
+    vector = vectorizer.transform([text])
+    return model.predict(vector)[0]
+
+st.title("Fake News Detection App")
+
+input_text = st.text_area("Enter News Text")
+
+if st.button("Check"):
+    result = predict_news(input_text)
+    
+    if result == "FAKE":
+        st.error("This News is FAKE ❌")
+    else:
+        st.success("This News is REAL ✅")
