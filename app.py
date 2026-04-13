c4f60891a8953d97974a7e7cfe9d30b718135712
Vijay240702 <vijaykumartsk24@gmail.com>
2026-04-13 15:02:39 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,12 @@
 import streamlit as st
 import pickle
 
+with open("model.pkl", "rb") as f:
+    model = pickle.load(f)
+
+with open("vectorizer.pkl", "rb") as f:
+    vectorizer = pickle.load(f)
+
 model = pickle.load(open("model.pkl", "rb"))
 vectorizer = pickle.load(open("vectorizer.pkl", "rb"))
 
@@ -19,3 +25,4 @@ if st.button("Check"):
         st.error("This News is FAKE ❌")
     else:
         st.success("This News is REAL ✅")
+        
